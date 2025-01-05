4201b1d47ee83ce5010c5031e6f7a06a1923ebaf
anubhav <anubhavbisht1994@gmail.com>
2025-01-05 11:15:17 +0530

    update

--- src/databaseConnection/jdbcconnection.java
+++ src/databaseConnection/jdbcconnection.java
@@ -1,12 +1,12 @@
 package databaseConnection;
-
+/*
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 //import java.time.Duration;
-
+*/
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,9 +15,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class jdbcconnection {
 
 	
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
-		/*
+		/*throws SQLException
 		TestNG testSuite = new TestNG();
 	    testSuite.setTestClasses(new Class[] { MyClass.class }); //it would be probably name of the class in which `main` method is located
 	   // testSuite.addListener(new MyListener()); //optional
@@ -25,7 +25,7 @@ public class jdbcconnection {
 	    testSuite.setDefaultTestName("Test Name");
 	    testSuite.setOutputDirectory("/my/output/dir");
 	    testSuite.run();
-	    */
+	   
 		String host="localhost";
 		String port="3306";
 		Connection con = DriverManager.getConnection("jdbc:mysql://" +host+ ":" +port+ "/demo", "root", "123456");
@@ -39,10 +39,17 @@ public class jdbcconnection {
 			driver.get("https://login.salesforce.com/");
 		driver.findElement(By.id("username")).sendKeys(rs.getString("username"));
 		driver.findElement(By.id("password")).sendKeys(rs.getString("password"));
+		
+		}
+		 */
 		//driver.findElement(By.id("Login")).click();
 		
 			//System.out.println(rs.getString("username"));
 		//System.out.println(rs.getString("password"));
-	}
+		System.setProperty("webdriver.chrome.driver", "E:\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://login.salesforce.com/");
+		driver.findElement(By.id("username")).sendKeys("user");
+		driver.findElement(By.id("password")).sendKeys("1234");
 	}
 }
